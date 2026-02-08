85431674189d3ce5f78bf30fde7c1c9e7e5fae5c
Wahaj Ul Haq <ulhaqwahaj@gmail.com>
2026-02-08 23:31:28 +0500

    fiex

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    dedupe: ['react', 'react-dom', 'react/jsx-runtime']
+  },
   server: {
-    allowedHosts: ['prayerapp-admin.wahaj.site', '192.168.18.7']
+    allowedHosts: ['prayerapp-admin.wahaj.site', '192.168.18.7'],
+    strictPort: false,
+    hmr: {
+      clientPort: 443,
+      protocol: 'wss'
+    }
   }
 })
